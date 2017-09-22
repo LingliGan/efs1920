707125bd2c7fa48b909f97b170c778946d071158
Lingli <lgan@unomaha.edu>
2017-09-21 20:32:06 -0500

    msg

--- efs/settings.py
+++ efs/settings.py
@@ -156,6 +156,21 @@ DATABASES['default'].update(db_from_env)
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+in_heroku = False
+if 'DATABASE_URL' in os.environ:
+    in_heroku = True
+
+import dj_database_url
+if in_heroku:
+    DATABASES = {'default': dj_database_url.config()}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 try:
     from .local_settings import *
